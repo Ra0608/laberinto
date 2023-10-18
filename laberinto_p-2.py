930cdc67fbf4c288c8cb8f4e9b1376c4473ccf0d
Ra0608 <145621361+Ra0608@users.noreply.github.com>
2023-10-17 21:21:28 -0500

    Update laberinto_p-2.py

--- laberinto_p-2.py
+++ laberinto_p-2.py
@@ -10,7 +10,7 @@
 from readchar import readkey, key
 # se importa las librerias a usar
 while True:
-    print("toca una tecal: ")
+    print("toca una tecla: ")
 #se le pide al usuario que oprima una letra
     letra=readkey()
     print(letra)
